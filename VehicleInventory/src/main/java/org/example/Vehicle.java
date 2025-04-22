80a3bbb1792de91db4c98dd0249dd444a51a05f1
Kevin Gagante <KevinGagante@Gmail.com>
2025-04-22 15:18:02 -0700

    created getters and setters for vehicle inventory

--- VehicleInventory/src/main/java/org/example/Vehicle.java
+++ VehicleInventory/src/main/java/org/example/Vehicle.java
@@ -7,6 +7,31 @@ public class Vehicle {
     private int odometerReading;
     private float price;
 
-    
+    public Vehicle( long _vehicleId, String _makeModel, String _color, int _odometerReading, float _price ) {
+        this.vehicleId = _vehicleId;
+        this.makeModel = _makeModel;
+        this.color = _color;
+        this.odometerReading = _odometerReading;
+        this.price = _price;
+    }
+
+    public long getVehicleId() { return vehicleId; }
+    public String getMakeModel() { return makeModel; }
+    public String getColor() { return color; }
+    public int getOdometerReading() { return odometerReading; }
+    public float getPrice() { return price; }
+
+    public void setVehicleId(long vehicleId) { this.vehicleId = vehicleId; }
+    public void setMakeModel(String makeModel) { this.makeModel = makeModel; }
+    public void setColor(String color) { this.color = color; }
+    public void setOdometerReading(int odometerReading) { this.odometerReading = odometerReading; }
+    public void setPrice(float price) { this.price = price; }
+
+
+
+
+
+
+
 
 }
