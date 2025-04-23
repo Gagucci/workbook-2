1663cd350925faec4bb573a30e624a6c59cfc7e7
Kevin Gagante <KevinGagante@Gmail.com>
2025-04-22 20:18:55 -0700

    Created Vehicle array and populated it

--- VehicleInventory/src/main/java/org/example/Main.java
+++ VehicleInventory/src/main/java/org/example/Main.java
@@ -4,6 +4,14 @@ public class Main {
     public static void main(String[] args) {
         Vehicle[] fullInventory = new Vehicle[20];
 
-        };
+        fullInventory[0] = new Vehicle(1, "Toyota Supra", "Blue", 50000, 20000);
+        fullInventory[1] = new Vehicle(2, "Honda S2000", "Red", 30000, 22000);
+        fullInventory[2] = new Vehicle(3, "Ford Focus", "Black", 40000, 18000);
+        fullInventory[3] = new Vehicle(4, "Chevrolet Malibu", "White", 60000, 21000);
+        fullInventory[4] = new Vehicle(5, "Nissan Altima", "Silver", 70000, 23000);
+        fullInventory[5] = new Vehicle(6, "Nissan 370Z", "Yellow", 80000, 25000);
+
+
+
     }
 }
